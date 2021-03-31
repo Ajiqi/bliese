ae0e98c8357c0101cd1272e5f213720e2184d0cf
Ajiqi <yuhaziq@gmail.com>
2021-03-31 22:52:55 +0800

    Added static root.

--- bliese/settings.py
+++ bliese/settings.py
@@ -121,5 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTH_USER_MODEL = 'bliese_api.UserProfile'
